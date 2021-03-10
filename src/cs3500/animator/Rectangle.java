00a835d69d19256425435bd79031e1f7595d0e2d
nbrisbois <brisbois.n@northeastern.edu>
2021-03-10 17:33:27 -0500

    Added IShape/Shape

--- src/cs3500/animator/Rectangle.java
+++ src/cs3500/animator/Rectangle.java
@@ -1,5 +1,33 @@
 package cs3500.animator;
 
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
 public class Rectangle extends Shape {
 
+  public Rectangle(Double pos, double h, double w, Color color,
+      int order) throws NullPointerException, IllegalArgumentException {
+    super(pos, h, w, color, order);
+  }
+
+  @Override
+  public void render() {
+
+  }
+
+  @Override
+  public void changePos(int pos) {
+
+  }
+
+  @Override
+  public void changeSize(int size) {
+
+  }
+
+  @Override
+  public int getPriority() {
+    return 0;
+  }
 }
--- src/cs3500/animator/Shape.java
+++ src/cs3500/animator/Shape.java
@@ -1,5 +1,32 @@
 package cs3500.animator;
 
+
+import java.awt.Color;
+import java.awt.geom.Point2D.Double;
+import java.util.Objects;
+
 public abstract class Shape implements IShape {
+
+  protected final Double position;
+  protected final double[] diamentions;
+  protected final Color color;
+  protected final int order;
+
+  public Shape(Double pos, double h, double w, Color color, int order) throws NullPointerException, IllegalArgumentException {
+    Objects.requireNonNull(pos);
+    Objects.requireNonNull(color);
+    if (h < 0 || h < 0) {
+      throw new IllegalArgumentException("The diamentions cannot be less than zero");
+    }
+    if (order < 0) {
+      //ToDO figure out how to implement order
+    }
+    this.position = new Double(pos.getX(), pos.getY());
+    diamentions = new double[]{h, w};
+    this.color = new Color(color.getRGB());
+    this.order = order;
+  }
+
+
 
 }

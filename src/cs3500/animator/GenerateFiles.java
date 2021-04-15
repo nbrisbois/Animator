2d23f0e576cb00b31813af5e87c10372cfd03d51
odelo <regan.col@northeastern.edu>
2021-04-14 21:06:08 -0400

    generate files updated

--- src/cs3500/animator/GenerateFiles.java
+++ src/cs3500/animator/GenerateFiles.java
@@ -5,6 +5,7 @@ import cs3500.animator.model.BasicAnimationModel;
 import cs3500.animator.model.IShape;
 import cs3500.animator.model.Motion;
 import cs3500.animator.model.Oval;
+import cs3500.animator.model.Rectangle;
 import java.awt.Color;
 import java.awt.geom.Point2D.Double;
 import java.io.FileWriter;
@@ -19,16 +20,37 @@ public class GenerateFiles {
 
   public static void main(String[] args) throws IOException {
 
-    Motion dropletFalling = new Motion(50, 400, Color.CYAN, 1, 1, 15);
+    Motion dropletFalling = new Motion(0, 400, Color.CYAN, 1, 1, 15);
     Motion ripple = new Motion(dropletFalling.getMoveX(), dropletFalling.getMoveY(), Color.BLUE, 1.5, 0, 10);
 
     Queue<Motion> rainMotions = new LinkedList<>();
     rainMotions.add(dropletFalling);
     rainMotions.add(ripple);
-
-    
-
     List shapes = new ArrayList<>();
+
+
+    Queue floorMotion = new LinkedList<>();
+    floorMotion.add(new Motion(0,0,Color.DARK_GRAY, 1, 1, 1000));
+    shapes.add(new Rectangle("floor", new Double(0, 400),
+        500,
+        400,
+        Color.DARK_GRAY,
+        0,
+        floorMotion,
+        0, 0));
+
+    Queue backgroundMotion = new LinkedList<>();
+    backgroundMotion.add(new Motion(0,0,Color.DARK_GRAY, 1, 1, 200));
+    backgroundMotion.add(new Motion(0,0,Color.CYAN, 1, 1, 2));
+    backgroundMotion.add(new Motion(0, 0, Color.CYAN, 1, 1, 200));
+    shapes.add(new Rectangle("background", new Double(0, 0),
+        500,
+        400,
+        Color.GRAY,
+        0,
+        backgroundMotion,
+        0, 0));
+
     for (int ii = 0; ii < 1000; ii++ ) {
       Random r = new Random();
       shapes.add(new Oval(
@@ -39,8 +61,9 @@ public class GenerateFiles {
           Color.CYAN,
           r.nextInt(100 - 1) + 1,
           rainMotions,
-          0, -10));
+          0, 0));
     }
+
     AnimationModel model = new BasicAnimationModel(shapes,  0, 0, 500, 500, 100, 1);
 
     renderModel(model);
@@ -50,8 +73,6 @@ public class GenerateFiles {
     FileWriter myWriter = new FileWriter(String.format("%s/%s", System.getProperty("user.dir"), "ripple.txt"));
 
     myWriter.write(String.format("canvas %s %s %s %s\n", model.getTopLeftX(), model.getTopLeftY(), model.getSceneHeight(), model.getSceneWidth()));
-
-    //TextualView view = new TextualView(model);
 
     for (IShape shape : model.getShapes()) {
       myWriter.write(shape.toString() + "\n \n");
